780829aa11a0a820d0671f448490416531f9da32
Anjali2104 <anjaliu949@gmail.com>
2023-09-27 22:38:30 +0530

    added getpost and getAllposts functionality

--- src/appwrite/conf.js
+++ src/appwrite/conf.js
@@ -50,6 +50,50 @@ export class Service{
          console.log("Appwrite service :: updatePost :: error", error)
        }
     }
+
+    async deletePost(slug){
+      try {
+        await this.databases.deleteDocument(
+            config.appwriteDatabaseId,
+            config.appwriteCollectionId,
+            slug,
+        )
+        return true;
+
+      } catch (error) {
+        console.log("Appwrite service :: deletePost :: error", error);
+        return false;
+      }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error", error);
+            return false;
+
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                [
+                    Query.equal("status", "active")
+                ]
+            )
+        } catch (error) {
+            console.log( console.log("Appwrite service :: getPosts :: error", error));
+            return false;
+        }
+    }
 }
 
 const service = new Service();
